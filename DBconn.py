e082d805ca3c5edb9b4e15a5c996e9bc2eaa9eca
rjhiles <ronnie@cyasupply.com>
2019-10-02 11:08:55 -0500

    add sqlite conn

--- DBconn.py
+++ DBconn.py
@@ -1,9 +1,10 @@
 # /usr/bin/python3
 
 import MySQLdb
+import sqlite3
 import os
 
-class DBConn():
+class DBConn:
     """
     Databse connection for timeclock with context manager.
     USE:        with DBconn() as conn:
@@ -11,14 +12,16 @@ class DBConn():
                     conn.commit()
     """
 
-    def __init__(self):
-        self.conn = MySQLdb.Connection(
-            host=os.environ['TIMECLOCK_DB_HOST'],
-            user=os.environ['TIMECLOCK_DB_USER'],
-            passwd=os.environ['TIMECLOCK_DB_PWD'],
-            port=3306,
-            db=os.environ['TIMECLOCK_DB_NAME'])
-
+    def __init__(self, type):
+        if type == 'mysql':
+            self.conn = MySQLdb.Connection(
+                host=os.environ['TIMECLOCK_DB_HOST'],
+                user=os.environ['TIMECLOCK_DB_USER'],
+                passwd=os.environ['TIMECLOCK_DB_PWD'],
+                port=3306,
+                db=os.environ['TIMECLOCK_DB_NAME'])
+        elif type == 'sqlite':
+            self.conn = sqlite3.connect('timeclock.db')
 
 
     def __enter__(self):
