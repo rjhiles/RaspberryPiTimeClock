7d53de84201cb6a4711ac09cba3c0846b30ce9e0
rjhiles <ronnie@cyasupply.com>
2019-10-04 15:23:44 -0500

    add logging restructured up0date_employee_table

--- Utils.py
+++ Utils.py
@@ -2,19 +2,30 @@
 
 from DBconn import DBConn
 from Database import *
+import logging
+import MySQLdb
+import datetime
+
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('Utils.log')
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(funcName)s:%(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 
 def update_employee_table():
-    with DBConn('sqlite') as conn:
-        c = conn.cursor()
-        c.execute("""DELETE FROM employee""")
-        conn.commit()
-    with DBConn('mysql') as conn:
-        employees = Employee(db_type='mysql', is_active=True).select_query()
-    with DBConn('sqlite') as sqlite_conn:
-        c = sqlite_conn.cursor()
-        for employee in employees:
-            query = """INSERT INTO employee VALUES {} """.format(employee)
-            c.execute(query)
-        sqlite_conn.commit()
-
+    # Retrieve from remote DB
+    try:
+        with DBConn('mysql') as conn:
+            employees = Employee(db_type='mysql', is_active=True).select_query()
+        with DBConn('sqlite') as sqlite_conn:
+            c = sqlite_conn.cursor()
+            c.execute("""DELETE FROM employee""")
+            sqlite_conn.commit()
+            for employee in employees:
+                query = """INSERT INTO employee VALUES {} """.format(employee)
+                c.execute(query)
+            sqlite_conn.commit()
+    except MySQLdb.Error as e:
+        logger.exception("Caught exception connecting to database\n Error: {}".format(e))
