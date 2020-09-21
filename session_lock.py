d3c0463cb9ec7b0b38f1856022669de34c7da458
rjhiles <ronnie@cyasupply.com>
2020-09-21 13:55:10 -0500

    prevent two instances of this app

---
+++ session_lock.py
@@ -0,0 +1,22 @@
+import os
+import psutil
+import sys
+
+TIME_CLOCK = "/tmp/time_clock.lock"
+
+
+def apply_lock():
+    with open(TIME_CLOCK, 'w') as file:
+        pid = str(os.getpid())
+        file.write(pid)
+
+
+def instance_running_check():
+    if os.path.exists(TIME_CLOCK):
+        with open(TIME_CLOCK, 'r') as lock_file:
+            lock_file_pid = lock_file.readline()
+        if lock_file_pid in psutil.pids():
+            # The process is running, kill this one
+            sys.exit("process is already running")
+        else:
+            apply_lock(lock_file)
